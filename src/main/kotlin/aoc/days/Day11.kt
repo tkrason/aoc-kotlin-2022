c9d82e712217c860d07f86bdd24a1c933a6b55d9
tkrason <tomas.krason@vacuumlabs.com>
2023-01-03 21:58:47 +0100

    Add solution for day 11 part 2
    Signed-off-by: tkrason <tomas.krason@vacuumlabs.com>

--- src/main/kotlin/aoc/days/Day11.kt
+++ src/main/kotlin/aoc/days/Day11.kt
@@ -1,25 +1,13 @@
 package aoc.days
 
 import util.FileLoader
+import java.math.BigDecimal
 
 val day11 = fun() {
 
     val text = FileLoader.loadFileAsText("/day11_input.txt")
 
-    val monkeys = text
-        .split("Monkey")
-        .drop(1) // fix for first value being empty
-        .map { it.split(System.lineSeparator()) }
-        .mapIndexed { index, splitLine ->
-            Monkey(
-                id = index,
-                items = splitLine[1].parseToItems(),
-                operation = splitLine[2].parseToOperation(),
-                test = splitLine[3].parseToTest(),
-                whenTrueThrowTo = splitLine[4].parseToMonkeyNumber(),
-                whenFalseThrowTo = splitLine[5].parseToMonkeyNumber()
-            )
-        }
+    val monkeys = text.parseMonkeys()
     monkeys.forEach { it.initTrueFalseMonkeys(monkeys) }
 
     repeat(20) { monkeys.simulateRound() }
@@ -30,12 +18,40 @@ val day11 = fun() {
         .map { it.inspectionCounter }
 
     println(topTwoThrowingMonkeys[0] * topTwoThrowingMonkeys[1])
+
+    //Part 2
+    val newMonkeys = text.parseMonkeys()
+    newMonkeys.forEach { it.initTrueFalseMonkeys(newMonkeys) }
+
+    repeat(10_000) { newMonkeys.simulateRound(applyWorryLevelDivision = false) }
+
+    val newTopTwoThrowingMonkeys = newMonkeys
+        .sortedByDescending { it.inspectionCounter }
+        .take(2)
+        .map { it.inspectionCounter }
+
+    println(newTopTwoThrowingMonkeys[0].toBigDecimal() * newTopTwoThrowingMonkeys[1].toBigDecimal())
 }
+
+private fun String.parseMonkeys() = this
+    .split("Monkey")
+    .drop(1) // fix for first value being empty
+    .map { it.split(System.lineSeparator()) }
+    .mapIndexed { index, splitLine ->
+        Monkey(
+            id = index,
+            items = splitLine[1].parseToItems(),
+            changeWorryLevel = splitLine[2].parseToOperation(),
+            divisionRemainder = splitLine[3].parseToTest(),
+            whenTrueThrowTo = splitLine[4].parseToMonkeyNumber(),
+            whenFalseThrowTo = splitLine[5].parseToMonkeyNumber()
+        )
+    }
 
 private fun String.parseToItems() = this
     .replace("Starting items: ", "")
     .split(", ")
-    .map { it.trim().toInt() }
+    .map { it.trim().toInt().toBigDecimal() }
     .toMutableList()
 
 private fun String.parseToOperation() = this
@@ -45,13 +61,13 @@ private fun String.parseToOperation() = this
         // Assuming null means that the last parameter is "old"
         when(split[6].toIntOrNull()) {
             null -> when (split[5]) {
-                "*" -> { old: Int -> old * old }
-                "+" -> { old: Int -> old + old }
+                "*" -> { old: BigDecimal -> old * old }
+                "+" -> { old: BigDecimal -> old + old }
                 else -> error("not supported operation!")
             }
             else -> when (split[5]) {
-                "*" -> { old: Int -> old * split[6].toInt() }
-                "+" -> { old: Int -> old + split[6].toInt() }
+                "*" -> { old: BigDecimal -> old * split[6].toBigDecimal() }
+                "+" -> { old: BigDecimal -> old + split[6].toBigDecimal() }
                 else -> error("not supported operation!")
             }
         }
@@ -59,7 +75,7 @@ private fun String.parseToOperation() = this
 
 private fun String.parseToTest() = this
     .split("divisible by ")
-    .let { split -> { value: Int -> value % split[1].toInt() == 0 } }
+    .let { split -> { value: BigDecimal -> value % split[1].toBigDecimal() } }
 
 private fun String.parseToMonkeyNumber() = this
     .split("monkey ")
@@ -68,9 +84,9 @@ private fun String.parseToMonkeyNumber() = this
 
 data class Monkey(
     val id: Int,
-    val items: MutableList<Int>,
-    val operation: (Int) -> Int,
-    val test: (Int) -> Boolean,
+    val items: MutableList<BigDecimal>,
+    val changeWorryLevel: (BigDecimal) -> BigDecimal,
+    val divisionRemainder: (BigDecimal) -> BigDecimal,
     val whenTrueThrowTo: Int,
     val whenFalseThrowTo: Int
 ) {
@@ -83,19 +99,22 @@ data class Monkey(
         falseThrowMonkey = list[whenFalseThrowTo]
     }
 
-    fun inspectAndThrowAll() {
+    fun inspectAndThrowAll(worryIsDividing : Boolean) {
         while (items.isNotEmpty()) {
             inspectionCounter++
-            inspectItemAndThrow(items.first())
+            when(worryIsDividing) {
+                true -> inspectItemAndThrow(items.first())
+                false -> inspectItemAndThrowWithoutWorryDivision(items.first())
+            }
         }
     }
 
-    private fun inspectItemAndThrow(worryLevel: Int) {
+    private fun inspectItemAndThrow(worryLevel: BigDecimal) {
 
-        val newWorryLevel = operation(worryLevel)
-        val afterMonkeyGetsBored = (newWorryLevel.toDouble() / 3).toInt()
+        val newWorryLevel = changeWorryLevel(worryLevel)
+        val afterMonkeyGetsBored = (newWorryLevel.toDouble() / 3).toInt().toBigDecimal()
 
-        when(test(afterMonkeyGetsBored)) {
+        when(divisionRemainder(afterMonkeyGetsBored) == BigDecimal.ZERO) {
             true -> trueThrowMonkey.catch(afterMonkeyGetsBored)
             false -> falseThrowMonkey.catch(afterMonkeyGetsBored)
         }
@@ -104,11 +123,24 @@ data class Monkey(
         items.removeFirst()
     }
 
-    private fun catch(itemWithWorryLevel: Int) {
+    private val leastCommonMultiple = BigDecimal("9699690")
+    private fun inspectItemAndThrowWithoutWorryDivision(worryLevel: BigDecimal) {
+        val newWorryLevel = changeWorryLevel(worryLevel)
+
+        when(divisionRemainder(newWorryLevel)) {
+            BigDecimal.ZERO -> trueThrowMonkey.catch(newWorryLevel % leastCommonMultiple)
+            else -> falseThrowMonkey.catch(newWorryLevel % leastCommonMultiple)
+        }
+
+        // Got thrown, can delete now
+        items.removeFirst()
+    }
+
+    private fun catch(itemWithWorryLevel: BigDecimal) {
         items.add(itemWithWorryLevel)
     }
 }
 
-fun List<Monkey>.simulateRound() {
-    this.forEach { it.inspectAndThrowAll() }
+fun List<Monkey>.simulateRound(applyWorryLevelDivision : Boolean = true) {
+    this.forEach { it.inspectAndThrowAll(applyWorryLevelDivision) }
 }
